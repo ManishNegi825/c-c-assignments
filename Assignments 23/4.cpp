dee0de6e76392cce9c6ed00b75a2d49f225f45c9
Manish Negi <108972949+ManishNegi825@users.noreply.github.com>
2022-08-26 17:27:52 +0530

    Add files via upload

---
+++ Assignments 23/4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+float r;
+cout<<"enter the value of the radius to know area of circle ";
+cin>>r;
+cout<<"area of a circle is "<<3.14*r*r;
+return 0;
+}
---
+++ Assignments 23/5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+float length,width,height;
+cout<<"enter the value of the length,width,height ";
+cin>>length>>width>>height;
+cout<<"The volume of a cuboid is  "<<length*width*height;
+return 0;
+}
