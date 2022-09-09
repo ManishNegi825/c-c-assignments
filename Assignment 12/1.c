c528d1d6b019607f26c493cc95b3b341a0ef84e5
Manish Negi <108972949+ManishNegi825@users.noreply.github.com>
2022-09-09 13:06:27 +0530

    Create 1.c

---
+++ Assignment 12/1.c
@@ -0,0 +1,16 @@
+/*Write a recursive function to print first N natural numbers */
+#include<stdio.h>
+void print(int n);
+int main()
+{
+ int n,s;
+ printf("Enter the value of n ");
+ scanf("%d",&n);
+ print(n);
+}
+void print(int n)
+{
+  if(n>1)
+  print(n-1);
+  printf("%d ",n);
+}
